1dff135cf8e6c5618f7e7fb8b58eac734b579394
Iain McKenzie <iainmck29@hotmail.com>
2018-09-10 18:32:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+
+let index = 0;
+
+const body = document.getElementById('body')
+ 
+body.addEventListener(codes, function(event) {
+  alert('I was clicked!')
+})
+function init(e) {
+  const key = e.key;
+  
+  if (key === codes[index]) {
+    index++;
+    
+    if (index === codes.length) {
+      alert('congrats');
+      
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
